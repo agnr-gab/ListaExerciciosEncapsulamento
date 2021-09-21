1338bad754c15a65842c5e4371607c9172ac55c6
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-21 20:45:16 -0300

    fazendo o encapsulamento exercicio 1

--- src/Exercicio1/ContaCorrente.java
+++ src/Exercicio1/ContaCorrente.java
@@ -1,2 +1,14 @@
-package Exercicio1;public class ContaCorrente {
+package Exercicio1;
+/*
+1. Modifique os acessos dos atributos da classe referente à Conta Bancária da aula anterior para private e execute o seu programa principal sem alterá-lo.
+ */
+public class ContaCorrente {
+    private String numeroConta;
+    private String nomeCorrentista;
+    private String saldo = "0";
+
+    public ContaCorrente(String numeroConta, String nomeCorrentista,String saldo) {
+        this.numeroConta = numeroConta;
+        this.nomeCorrentista = nomeCorrentista;
+        this.saldo = saldo;
 }
--- src/Exercicio1/Main.java
+++ src/Exercicio1/Main.java
@@ -1,2 +1,7 @@
-package Exercicio1;public class Main {
+package Exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
