7845e3da15e70131d3134c2382820327a1ffa8eb
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-21 21:14:14 -0300

    finalizado exercicio 1 com encapsulamento

--- src/Exercicio1/Main.java
+++ src/Exercicio1/Main.java
@@ -2,6 +2,14 @@ package Exercicio1;
 
 public class Main {
     public static void main(String[] args) {
-
+        ContaCorrente correntista1 = new ContaCorrente("0256000-14", "Rogil Zozei Haitiar", 1569);
+        correntista1.extrato();
+        System.out.println("------------------");
+        correntista1.setNomeCorrentista("Vuipial Erbas Goans");
+        correntista1.setNumeroConta("010236589-74");
+        correntista1.extrato();
+        System.out.println("------------------");
+        correntista1.deposito(16984);
+        correntista1.extrato();
     }
 }
